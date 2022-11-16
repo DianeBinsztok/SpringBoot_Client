3efb83c33856a005358a10bbec509b031899c4b8
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-11-16 14:19:26 +0100

    feat: client fetches index on home url

---
+++ client/src/main/java/web/car/client/controllers/CarController.java
@@ -0,0 +1,24 @@
+package web.car.client.controllers;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import java.util.List;
+
+//@JsonIgnoreProperties(ignoreUnknown = true) => ça sert à quoi?
+@RestController
+public class CarController {
+
+    private String url = "http://localhost:8080";
+
+    @GetMapping("/")
+    public Object getIndex(){
+        RestTemplate restTemplate = new RestTemplate();
+        Object index = restTemplate.getForObject(this.url, String.class);
+        System.out.println("La réponse à mon getIndex =>" + index);
+        return index;
+    }
+}
